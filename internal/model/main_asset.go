23fbd7a8aa147e2d53084889f855774e3bbd23dd
Alerio Reyhan <alerioreyhan127@gmail.com>
2024-06-16 10:20:56 +0700

    chore: add timestamp field

--- internal/model/main_asset.go
+++ internal/model/main_asset.go
@@ -1,14 +1,22 @@
 package model
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type MainAsset struct {
 	ID           uuid.UUID `json:"id" gorm:"type:uuid;primarykey;default:gen_random_uuid()"`
 	DocumentID   uuid.UUID `json:"document_id"`
 	Document     Document
-	Category     string `json:"category" gorm:"varchar(128)"`
-	AssetTotal   int8   `json:"asset_total" gorm:"int8"`
-	District     string `json:"district" gorm:"varchar(255)"`
-	SubDistrict  string `json:"sub_district" gorm:"varchar(255)"`
-	UrbanVillage string `json:"urban_village" gorm:"varchar(255)"`
+	Category     string         `json:"category" gorm:"varchar(128)"`
+	AssetTotal   int8           `json:"asset_total" gorm:"int8"`
+	District     string         `json:"district" gorm:"varchar(255)"`
+	SubDistrict  string         `json:"sub_district" gorm:"varchar(255)"`
+	UrbanVillage string         `json:"urban_village" gorm:"varchar(255)"`
+	DeletedAt    gorm.DeletedAt `json:"-" gorm:"index"`
+	CreatedAt    time.Time      `json:"created_at"`
+	UpdatedAt    time.Time      `json:"updated_at"`
 }
